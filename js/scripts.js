316366f96ff96acd48c2c6536757ee6061e3054a
Zeeshan Al Abedin <zeeshanalabedin@gmail.com>
2018-01-18 12:50:54 +0530

    Added Event_remove file, updated scripts to include Delete Event as well

--- js/scripts.js
+++ js/scripts.js
@@ -174,11 +174,39 @@ techtalk.controller('AddEventController', function ($scope, $http) {
         };
         $http.post(APIURL + '/PostEventTable', eventObject)
             .then(function successCallback() {
+                alert("Addition Successful!");
                 console.log("Success");
             }, function errorCallback() {
+                alert("Something Went Wrong");
                 console.log("Failed");
             });
     };
 });
 
+//Remove Event Page Controller
+techtalk.controller('RemoveEventController', function ($scope, $http) {
+    $scope.getAllEvents = function () {
+        $http.get(APIURL + '/GetAllEvents')
+            .then(function successCallback(response) {
+                $scope.rows = response.data;
+            }, function errorCallback(response) {
+                console.log("Unable to perform get request");
+            });
+    };
 
+    $scope.deleteEvent = function (eventID) {
+        var idPara = $.param({
+            id: eventID
+        });
+        console.log(idPara);
+        $http.delete(APIURL + '/DeleteEventTable?' + idPara)
+            .success(function () {
+                $scope.getAllEvents();
+                console.log("deleted");
+            })
+            .error(function () {
+                alert("Something went wrong");
+                console.log("An error occured!");
+            });
+    };
+});
